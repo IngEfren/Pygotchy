d06ee8826b065860f92f40c14167c08ed9bcd69d
IngEfren <157951326+IngEfren@users.noreply.github.com>
2024-02-03 02:27:27 -0600

    Screen shot test

--- Main.py
+++ Main.py
@@ -5,3 +5,10 @@
 
 # Modulo para trabajar con funciones de la pantalla
 # pip install Pillow
+
+# Importamos modulo para trabajar con funciones del sistema
+import pyautogui
+
+# Tomamos una captura de pantalla almacenada en la ruta dada
+pyautogui.screenshot(r"C:\Users\edgar\Downloads\screenshot1.png")
+
